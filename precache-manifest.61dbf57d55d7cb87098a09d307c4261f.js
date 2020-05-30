db21b18fcd71d9bbc362b57d7cae677a53466eb4
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-05-29 18:30:02 -0600

    Updates

---
+++ precache-manifest.61dbf57d55d7cb87098a09d307c4261f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6234d3a328f8bebc920a316256bebc",
+    "url": "/react_portfolio/a2af41b132d3a07c923a.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/react_portfolio/a2af41b132d3a07c923a.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf03e34b69d56db4b54ab4b6605b9b0",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "c047d8d351cd3cd573e5",
+    "url": "/react_portfolio/static/css/main.836b50cd.chunk.css"
+  },
+  {
+    "revision": "ef011c42dc719471f7fa",
+    "url": "/react_portfolio/static/js/2.e5517e57.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/react_portfolio/static/js/2.e5517e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c047d8d351cd3cd573e5",
+    "url": "/react_portfolio/static/js/main.560ed29b.chunk.js"
+  },
+  {
+    "revision": "e3e9b940af44bc849f7c",
+    "url": "/react_portfolio/static/js/runtime-main.db4a5409.js"
+  },
+  {
+    "revision": "c8221b3b5bb8aa5ae9ea8cdcfc63517f",
+    "url": "/react_portfolio/static/media/Resume.c8221b3b.pdf"
+  }
+]);
