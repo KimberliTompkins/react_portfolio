808556a94b77e83e569133e72bf37324316a4fc5
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-06-09 00:36:52 -0600

    added more text to hero componet

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,14 +5,16 @@ import NavTabs from "../NavTabs/index";
 function Hero() {
   return (
     <section class="bgimage">
-      <NavTabs/>
+      <NavTabs />
       <div class="container-fluid">
         <div class="row">
           <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 p-2">
             <div class="float-right">
               <h1 class="headliner">Kimberli Tompkins</h1>
               <h3 class="headliner">Full Stack Web Developer</h3>
-
+              <h5 class="headliner">with years of experience with both</h5>
+              <h5 class="headliner">Front End and Back End development.</h5>
+              
             </div>
           </div>
           <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 ">
@@ -21,15 +23,22 @@ function Hero() {
               class="card-img m-3 portImage"
               alt="thisisme"
             />
-                            <a href="https://github.com/KimberliTompkins" class="list-group-item-action m-2 headliner"target="_blank">
-                  <ion-icon name="logo-github"></ion-icon> Github
-                </a>
-                <a href="https://www.linkedin.com/in/kimberli-tompkins-4a2443a5/?
-lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BtaeOTjrCScul0HYK2stk6A%3D%3D" class="list-group-item-action headliner" 
-target="_blank">
-                  <ion-icon name="logo-linkedin"></ion-icon> Linkedin
-                </a>
-         
+            <a
+              href="https://github.com/KimberliTompkins"
+              class="list-group-item-action m-2 headliner"
+              target="_blank"
+            >
+              <ion-icon name="logo-github"></ion-icon> Github
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kimberli-tompkins-4a2443a5/?
+lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BtaeOTjrCScul0HYK2stk6A%3D%3D"
+              class="list-group-item-action headliner"
+              target="_blank"
+            >
+              <ion-icon name="logo-linkedin"></ion-icon> Linkedin
+            </a>
+            <br />
           </div>
         </div>
       </div>
